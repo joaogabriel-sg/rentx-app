e4b841e31c28b7970417f021df5e0a5a682cd0bf
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-04 21:52:02 -0300

    feat(theme): create app theme

---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: 30px;
+`;
