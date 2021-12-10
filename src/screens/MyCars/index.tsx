b8685832fa3d63dd198be6186460b07855544872
João Gabriel <main.joaogabrielsg@gmail.com>
2021-12-09 22:27:22 -0300

    feat: render user scheduled cars

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FlatList, StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
+import {
+  NavigationProp,
+  ParamListBase,
+  useNavigation,
+} from "@react-navigation/native";
+
+import { BackButton, Car, Load } from "../../components";
+
+import { api } from "../../services";
+
+import { CarDTO } from "../../dtos";
 
 import * as S from "./styles";
 
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+}
+
 export function MyCars() {
-  return <S.Container></S.Container>;
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const theme = useTheme();
+  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get(`/schedules_byuser?user_id=1`);
+        console.log(response.data);
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
+
+  return (
+    <S.Container>
+      <S.Header>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <BackButton color={theme.colors.shape} onPress={handleBack} />
+
+        <S.Title>
+          Seus agendamentos,{"\n"}
+          estão aqui
+        </S.Title>
+
+        <S.Subtitle>Conforto, segurança e praticidade.</S.Subtitle>
+      </S.Header>
+
+      {loading ? (
+        <Load />
+      ) : (
+        <S.Content>
+          <S.Appointments>
+            <S.AppointmentsTitle>Agendamentos feitos</S.AppointmentsTitle>
+            <S.AppointmentsQuantity>
+              {`${cars.length}`.padStart(2, "0")}
+            </S.AppointmentsQuantity>
+          </S.Appointments>
+
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <S.CarWrapper>
+                <Car data={item.car} />
+                <S.CarFooter>
+                  <S.CarFooterTitle>Período</S.CarFooterTitle>
+                  <S.CarFooterPeriod>
+                    <S.CarFooterDate>{item.startDate}</S.CarFooterDate>
+                    <AntDesign
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <S.CarFooterDate>{item.endDate}</S.CarFooterDate>
+                  </S.CarFooterPeriod>
+                </S.CarFooter>
+              </S.CarWrapper>
+            )}
+          />
+        </S.Content>
+      )}
+    </S.Container>
+  );
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,106 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
+  background: ${({ theme }) => theme.colors.background_primary};
+
   flex: 1;
+  align-items: center;
+`;
+
+export const Header = styled.View`
+  background: ${({ theme }) => theme.colors.header};
+  width: 100%;
+  padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    margin-top: 24px;
+
+    font-family: ${theme.fonts.secondary_600};
+    font-size: ${RFValue(30)}px;
+    color: ${theme.colors.shape};
+  `}
+`;
+
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    margin-top: 24px;
+
+    font-family: ${theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+    color: ${theme.colors.shape};
+  `}
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  padding: 0 16px;
+  flex: 1;
+`;
+
+export const Appointments = styled.View`
+  width: 100%;
+  padding: 24px 0;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const AppointmentsTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_400};
+    font-size: ${RFValue(15)}px;
+    color: ${theme.colors.text};
+  `}
+`;
+
+export const AppointmentsQuantity = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    font-size: ${RFValue(15)}px;
+    color: ${theme.colors.title};
+  `}
+`;
+
+export const CarWrapper = styled.View`
+  margin-bottom: 16px;
+`;
+
+export const CarFooter = styled.View`
+  background: ${({ theme }) => theme.colors.background_secondary};
+  width: 100%;
+  padding: 12px;
+  margin-top: -10px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CarFooterTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.secondary_500};
+    font-size: ${RFValue(10)}px;
+    color: ${theme.colors.text_detail};
+  `}
+`;
+
+export const CarFooterPeriod = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CarFooterDate = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_400};
+    font-size: ${RFValue(13)}px;
+    color: ${theme.colors.title};
+  `}
 `;
