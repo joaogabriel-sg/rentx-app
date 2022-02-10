5a15a69107ad423f978258ef46d5188d44274d88
João Gabriel <main.joaogabrielsg@gmail.com>
2022-02-10 14:49:02 -0300

    feat: add avatar picker

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -12,6 +12,7 @@ import {
   useNavigation,
 } from "@react-navigation/native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import * as ImagePicker from "expo-image-picker";
 
 import { BackButton, Input, PasswordInput } from "../../components";
 
@@ -20,9 +21,13 @@ import { useAuth } from "../../hooks/auth";
 import * as S from "./styles";
 
 export function Profile() {
-  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
-
   const { user } = useAuth();
+
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
+
   const theme = useTheme();
   const navigation = useNavigation<NavigationProp<ParamListBase>>();
 
@@ -34,6 +39,19 @@ export function Profile() {
 
   function handleOptionChange(optionSelected: "dataEdit" | "passwordEdit") {
     setOption(optionSelected);
+  }
+
+  async function handleAvatarSelect() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (result.cancelled) return;
+
+    if (result.uri) setAvatar(result.uri);
   }
 
   return (
@@ -50,10 +68,8 @@ export function Profile() {
             </S.HeaderTop>
 
             <S.PhotoContainer>
-              <S.Photo
-                source={{ uri: "https://github.com/joaogabriel-sg.png" }}
-              />
-              <S.PhotoButton onPress={() => {}}>
+              {!!avatar && <S.Photo source={{ uri: avatar }} />}
+              <S.PhotoButton onPress={handleAvatarSelect}>
                 <Feather name="camera" size={24} color={theme.colors.shape} />
               </S.PhotoButton>
             </S.PhotoContainer>
@@ -87,6 +103,7 @@ export function Profile() {
                   autoCapitalize="none"
                   autoCorrect={false}
                   defaultValue={user.name}
+                  onChangeText={setName}
                 />
                 <Input
                   iconName="mail"
@@ -98,6 +115,7 @@ export function Profile() {
                   placeholder="CNH"
                   keyboardType="numeric"
                   defaultValue={user.driver_license}
+                  onChangeText={setDriverLicense}
                 />
               </S.Section>
             )}
