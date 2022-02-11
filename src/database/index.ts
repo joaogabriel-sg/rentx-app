6bf4aadc3940ede62306c1a87cac7832a8ff9e3e
João Gabriel <main.joaogabrielsg@gmail.com>
2022-02-11 10:21:22 -0300

    feat: update database adding car model and schema

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,9 +4,10 @@ import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
 import { schemas } from "./schema";
 
 import { User } from "./model/User";
+import { Car } from "./model/Car";
 
 const adapter = new SQLiteAdapter({
   schema: schemas,
 });
 
-export const database = new Database({ adapter, modelClasses: [User] });
+export const database = new Database({ adapter, modelClasses: [User, Car] });
